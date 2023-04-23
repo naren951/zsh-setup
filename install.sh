ed091b3e2c3d4fe52930b7f9797f51b1af9e4e7c
Narendiran Arthanarieswaran <73490616+naren951@users.noreply.github.com>
2023-04-23 22:58:54 +0530

    Updated install.sh

--- install.sh
+++ install.sh
@@ -4,12 +4,14 @@ ZSH_PACK_DIR=$HOME/packages/zsh
 ZSH_LINK="https://sourceforge.net/projects/zsh/files/latest/download"
 
 # Installing ncurses
+
 curl -Lo "ncurses-6.1.tar.gz" "ftp://ftp.gnu.org/gnu/ncurses/ncurses-6.1.tar.gz"
 tar xvf "ncurses-6.1.tar.gz"
 rm "ncurses-6.1.tar.gz"
 cd "ncurses-6.1"
 ./configure --prefix=$HOME/local CXXFLAGS="-fPIC" CFLAGS="-fPIC"
 make -j && make install
+
 # Creates ZSH package directory
 if [[ ! -d "$ZSH_PACK_DIR" ]]; then
     echo "Creating zsh directory under packages directory"
@@ -21,7 +23,7 @@ if [[ ! -f $ZSH_SRC_NAME ]]; then
 fi
 
 # Extracting ZSH
-tar xvf "$ZSH_SRC_NAME" "$ZSH_PACK_DIR"
+tar -xJvf "$ZSH_SRC_NAME" -C "$ZSH_PACK_DIR" --strip-components 1
 cd "$ZSH_PACK_DIR"
 
 # Configuring and Installing ZSH
@@ -31,15 +33,16 @@ cd "$ZSH_PACK_DIR"
 make -j && make install
 
 echo "export PATH=$HOME/local/bin:$PATH
-export SHELL=`which zsh`
-[ -f \"$SHELL\" ] && exec \"$SHELL\" -l" >> ~/.bash_profile
+export SHELL=$(which zsh)
+[ -f ${SHELL} ] && exec ${SHELL} -l" > ~/.bash_profile
 
 source ~/.bash_profile
 
 echo "installing oh-my-zsh"
 # Installing oh-my-zsh
-curl -Lo https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh install-ohmyzsh.sh
+curl -Lo install-ohmyzsh.sh https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh
 sh -c install-ohmyzsh.sh
+rm install-ohmyzsh.sh
 # rm install-ohmyzsh.sh
 
 # Downloading ZSH Auto Suggestions
@@ -47,11 +50,11 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
-# echo 'export ZSH="$HOME/.oh-my-zsh"
-# ZSH_THEME="powerlevel9k/powerlevel9k"
-# plugins=( git zsh-autosuggestions zsh-syntax-highlighting)
-# POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir vcs)
-# POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs)
-# ' > ~/.zshrc
-# source ~/.zshrc
-# plugins=(git)
+
+echo 'export ZSH="$HOME/.oh-my-zsh"
+ZSH_THEME="powerlevel9k/powerlevel9k"
+plugins=( git zsh-autosuggestions zsh-syntax-highlighting )
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir vcs)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs)
+source $ZSH/oh-my-zsh.sh' > ~/.zshrc
+source ~/.zshrc
